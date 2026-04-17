1e9535e267795bbac75a0b8a9ff6757e761939d1
SANA SRIVALLI <143521609+srivalliSana@users.noreply.github.com>
2026-04-17 13:59:02 +0530

    Update ai.js

--- routes/ai.js
+++ routes/ai.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import jwt from 'jsonwebtoken';
-import User from '../models/User.js';
+import User from '../models/user.js';
 import dotenv from 'dotenv';
 
 dotenv.config();
